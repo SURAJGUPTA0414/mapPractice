df571a4b0653091cf7e4987eba80d7575606ceca
SURAJGUPTA0414 <sandygupta0414@gmail.com>
2022-10-07 14:21:18 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Entry
+            id={emojipedia.id}
+            key={emojipedia.id}
+            emoji={emojipedia.emoji}
+            name={emojipedia.name}
+            meaning={emojipedia.meaning}
+          />
+        ))}
+
+        {/* <Entry 
+        emoji ={emojipedia[0].emoji}
+        name ={emojipedia[0].name}
+        meaning ={emojipedia[0].meaning}
+        />
+        <Entry 
+          emoji ={emojipedia[1].emoji}
+        name ={emojipedia[1].name}
+        meaning ={emojipedia[1].meaning}
+        />
+        <Entry
+        emoji ={emojipedia[2].emoji}
+        name ={emojipedia[2].name}
+      meaning ={emojipedia[2].meaning}
+         /> */}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
